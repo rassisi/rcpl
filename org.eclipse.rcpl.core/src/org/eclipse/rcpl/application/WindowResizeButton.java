37b9488ec9499a709e707bd60fdce7173dec0411
Ramin <Ramin@DESKTOP-69V2J7P>
2018-11-11 11:16:03 +0100

    a

--- org.eclipse.rcpl.core/src/org/eclipse/rcpl/application/WindowResizeButton.java
+++ org.eclipse.rcpl.core/src/org/eclipse/rcpl/application/WindowResizeButton.java
@@ -120,73 +120,77 @@ public class WindowResizeButton extends Region {
 			@Override
 			public void handle(MouseEvent e) {
 
-				if (stage != null) {
-					ObservableList<Screen> screens = Screen.getScreensForRectangle(stage.getX(), stage.getY(), 1, 1);
-					final Screen screen;
-					if (screens.size() > 0) {
-						screen = Screen.getScreensForRectangle(stage.getX(), stage.getY(), 1, 1).get(0);
+				try {
+					if (stage != null) {
+						ObservableList<Screen> screens = Screen.getScreensForRectangle(stage.getX(), stage.getY(), 1,
+								1);
+						final Screen screen;
+						if (screens.size() > 0) {
+							screen = Screen.getScreensForRectangle(stage.getX(), stage.getY(), 1, 1).get(0);
+						} else {
+							screen = Screen.getScreensForRectangle(0, 0, 1, 1).get(0);
+						}
+						Rectangle2D visualBounds = screen.getVisualBounds();
+
+						double maxX;
+						double maxY;
+						double minY;
+
+						double w = 0;
+						double h = 0;
+
+						if (Cursor.SE_RESIZE.equals(type)) {
+							maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
+							maxY = Math.min(visualBounds.getMaxY(), e.getScreenY() - dragOffsetY);
+							w = Math.max(stageMinimumWidth, maxX - stage.getX());
+							h = Math.max(stageMinimumHeight, maxY - stage.getY()) + 10;
+							update(w, h);
+						} else if (Cursor.S_RESIZE.equals(type)) {
+							maxY = Math.min(visualBounds.getMaxY(), e.getScreenY() - dragOffsetY);
+							h = Math.max(stageMinimumHeight, maxY - stage.getY()) + 3;
+							update(w, h);
+						} else if (Cursor.NE_RESIZE.equals(type)) {
+							maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
+							minY = Math.max(visualBounds.getMinY(), e.getScreenY() - dragOffsetY);
+							h = Math.max(stageMinimumHeight, startMaxY - stage.getY());
+							w = Math.max(stageMinimumWidth, maxX - stage.getX());
+							update(w, h);
+							stage.setY(minY - 10);
+
+						} else if (Cursor.N_RESIZE.equals(type)) {
+							minY = Math.max(visualBounds.getMinY(), e.getScreenY() - dragOffsetY);
+							h = Math.max(stageMinimumHeight, startMaxY - stage.getY());
+							update(w, h);
+							stage.setY(minY - 6);
+						} else if (Cursor.E_RESIZE.equals(type)) {
+							maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
+							w = Math.max(stageMinimumWidth, maxX - stage.getX());
+							update(w, h);
+						} else if (Cursor.W_RESIZE.equals(type)) {
+							maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
+							w = Math.max(stageMinimumWidth, maxX - stage.getX());
+							update(w, h);
+						}
+
 					} else {
-						screen = Screen.getScreensForRectangle(0, 0, 1, 1).get(0);
-					}
-					Rectangle2D visualBounds = screen.getVisualBounds();
+						double scale = WindowResizeButton.this.editor.getScale();
 
-					double maxX;
-					double maxY;
-					double minY;
-
-					double w = 0;
-					double h = 0;
-
-					if (Cursor.SE_RESIZE.equals(type)) {
-						maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
-						maxY = Math.min(visualBounds.getMaxY(), e.getScreenY() - dragOffsetY);
-						w = Math.max(stageMinimumWidth, maxX - stage.getX());
-						h = Math.max(stageMinimumHeight, maxY - stage.getY()) + 10;
-						update(w, h);
-					} else if (Cursor.S_RESIZE.equals(type)) {
-						maxY = Math.min(visualBounds.getMaxY(), e.getScreenY() - dragOffsetY);
-						h = Math.max(stageMinimumHeight, maxY - stage.getY()) + 3;
-						update(w, h);
-					} else if (Cursor.NE_RESIZE.equals(type)) {
-						maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
-						minY = Math.max(visualBounds.getMinY(), e.getScreenY() - dragOffsetY);
-						h = Math.max(stageMinimumHeight, startMaxY - stage.getY());
-						w = Math.max(stageMinimumWidth, maxX - stage.getX());
-						update(w, h);
-						stage.setY(minY - 10);
-
-					} else if (Cursor.N_RESIZE.equals(type)) {
-						minY = Math.max(visualBounds.getMinY(), e.getScreenY() - dragOffsetY);
-						h = Math.max(stageMinimumHeight, startMaxY - stage.getY());
-						update(w, h);
-						stage.setY(minY - 6);
-					} else if (Cursor.E_RESIZE.equals(type)) {
-						maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
-						w = Math.max(stageMinimumWidth, maxX - stage.getX());
-						update(w, h);
-					} else if (Cursor.W_RESIZE.equals(type)) {
-						maxX = Math.min(visualBounds.getMaxX(), e.getScreenX() + dragOffsetX);
-						w = Math.max(stageMinimumWidth, maxX - stage.getX());
+						double diffX = (e.getSceneX() - dragOffsetX - 2) / scale;
+						double diffY = (e.getSceneY() - dragOffsetY) / scale;
+						double w = startWidth + diffX;
+						double h = startHeight + diffY;
+						w = Math.max(1, w);
+						h = Math.max(1, h);
+						if (e.isControlDown()) {
+							h = w / ratio;
+						}
 						update(w, h);
 					}
 
-				} else {
-					double scale = WindowResizeButton.this.editor.getScale();
-
-					double diffX = (e.getSceneX() - dragOffsetX - 2) / scale;
-					double diffY = (e.getSceneY() - dragOffsetY) / scale;
-					double w = startWidth + diffX;
-					double h = startHeight + diffY;
-					w = Math.max(1, w);
-					h = Math.max(1, h);
-					if (e.isControlDown()) {
-						h = w / ratio;
-					}
-					update(w, h);
+					e.consume();
+				} catch (Exception ex) {
+					Rcpl.get().printErrorln("", ex);
 				}
-
-				e.consume();
-
 			}
 		});
 	}
