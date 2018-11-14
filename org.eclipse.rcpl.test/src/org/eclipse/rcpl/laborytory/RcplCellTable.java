4a3953d891e65e2cac2e60cc483e61a60632d567
Ramin <Ramin@DESKTOP-69V2J7P>
2018-11-14 20:42:09 +0100

    q

--- org.eclipse.rcpl.test/src/org/eclipse/rcpl/laborytory/RcplCellTable.java
+++ org.eclipse.rcpl.test/src/org/eclipse/rcpl/laborytory/RcplCellTable.java
@@ -36,14 +36,19 @@ public class RcplCellTable {
 
 		node.setStyle("-fx-border-color: blue;-fx-border-width: 1;");
 		for (int row = 0; row < this.table.getRowCount(); row++) {
-			for (int column = 0; column < table.getColumnCount(); column++) {
-				StackPane sp = new StackPane();
-				sp.setPrefSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
-				sp.setMinSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
-				sp.setMaxSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
-				grid.add(sp, column, row);
-			}
+			StackPane sp = new StackPane();
+			sp.setPrefSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
+			sp.setMinSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
+			sp.setMaxSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
+			grid.add(sp, 0, row);
+		}
 
+		for (int column = 0; column < table.getColumnCount(); column++) {
+			StackPane sp = new StackPane();
+			sp.setPrefSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
+			sp.setMinSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
+			sp.setMaxSize(IRcplTableConstants.DEFAULT_CELL_WIDTH, IRcplTableConstants.DEFAULT_ROW_HEIGHT);
+			grid.add(sp, column, 0);
 		}
 
 	}
